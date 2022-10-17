76b9e661c8f702caf1ef360336b4ce700ba9876c
unknown <vinithkaradi747@gmail.com>
2022-10-17 11:53:48 +0530

    Fixed bugs and changed design

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ for(let btn of btn_arr) {
     btn.addEventListener('click',()=>{
         let num1span = document.getElementById('num1');
         let num2span = document.getElementById('num2');
+        let opspan = document.getElementById('op');
         if(btn.className == 'btn numbers'){
-            if(document.getElementById('op').innerText == ''){
+            if(opspan.innerText == ''){
                 if(btn.innerText == '.' && num1span.innerText.includes('.')){
                     //pass
                 } else {
@@ -30,46 +31,54 @@ for(let btn of btn_arr) {
                     //pass
                 } else {
                     num2span.innerText += btn.innerText;
-                    let res = operate(document.getElementById('op').innerText,
+                    let res = operate(opspan.innerText,
                     parseFloat(num1span.innerText),
                     parseFloat(num2span.innerText));
-                    console.log(document.getElementById('op').innerText);
                     res = Math.round(res * 1e7) / 1e7;
-                    if(res > 1e7 || res < 1e-7){
+                    if(res > 1e7 || (res < 1e-7 && res > -1e-7)){
                         res = res.toExponential(7);
                     }
                     document.getElementById('answer').innerText = res;
                 };
             };
         } else if(btn.className == 'btn operator'){
-            if(num1span.innerText != '' && num2span.innerText == ''){
-                document.getElementById('op').innerText = btn.innerText;
+            if(btn.innerText == '-'){
+                if(num1span.innerText == ''){
+                    num1span.innerText += '-';
+                }else if(opspan.innerText == '^' && num2span.innerText == ''){
+                    num2span.innerText += '-';
+                } else {
+                    opspan.innerText = '-';
+                }
+            } else if(num1span.innerText != '' && num2span.innerText == ''){
+                opspan.innerText = btn.innerText;
             };
         } else if(btn.className == 'btn evaluate'){
-            num1span.innerText = document.getElementById('answer').innerText;
-            document.getElementById('answer').innerText = '';
-            document.getElementById('op').innerText = '';
-            num2span.innerText = '';
+            if(num2span.innerText != ''){
+                num1span.innerText = document.getElementById('answer').innerText;
+                document.getElementById('answer').innerText = '';
+                opspan.innerText = '';
+                num2span.innerText = '';
+            }
         } else if(btn.className == 'btn del'){
             let len = 0;
             if(num2span.innerText != ''){
                 len = num2span.innerText.length;
                 num2span.innerText = num2span.innerText.slice(0,len-1);
                 if(num2span.innerText != ''){
-                    let res = operate(document.getElementById('op').innerText,
+                    let res = operate(opspan.innerText,
                         parseFloat(num1span.innerText),
                         parseFloat(num2span.innerText));
-                    console.log(document.getElementById('op').innerText);
                     res = Math.round(res * 1e7) / 1e7;
-                    if(res > 1e7 || res < 1e-7){
+                    if(res > 1e7 || (res < 1e-7 && res > -1e-7)){
                         res = res.toExponential(7);
                     }
                     document.getElementById('answer').innerText = res;
                 } else {
                     document.getElementById('answer').innerText = '';
                 }
-            } else if(document.getElementById('op').innerText != ''){
-                document.getElementById('op').innerText = '';
+            } else if(opspan.innerText != ''){
+                opspan.innerText = '';
             } else if(num1span.innerText != ''){
                 len = num1span.innerText.length;
                 num1span.innerText = num1span.innerText.slice(0,len-1);
